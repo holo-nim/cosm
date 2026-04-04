d75b9a1bcd84a4e97054dd1dfa26545cfaeeeea2
metagn <metagngn@gmail.com>
2026-04-04 16:08:44 +0300

    different case algorithm, should also handle parseHTML etc

--- src/cosm/caseutils.nim
+++ src/cosm/caseutils.nim
@@ -1,19 +1,42 @@
 import std/strutils
 
-proc toSnakeCase*(s: string): string =
-  # originally written by treeform in jsony https://github.com/treeform/jsony
-  if s.len == 0:
-    return
-  var prevCap = false
-  for i, c in s:
-    if c in {'A'..'Z'}:
-      if i == 0:
-        result.add c
+iterator splitWordsCamelCase*(s: string, uncapitalized: bool = false): string =
+  if s.len != 0:
+    var current: string
+    if uncapitalized: current = $toLowerAscii(s[0])
+    else: current = $s[0]
+    var i = 1
+    while i < s.len:
+      var c = s[i]
+      if isUpperAscii(c):
+        c = toLowerAscii(c)
+        if isLowerAscii(s[i-1]) or (i + 1 < s.len and isLowerAscii(s[i+1])):
+          yield current
+          current = $c
+        else:
+          current.add(c)
+      elif c in Whitespace:
+        if current.len != 0:
+          yield current
+          current = ""
+      elif c == '_':
+        if current.len != 0:
+          yield current
+          current = ""
       else:
-        if result[result.len-1] != '_' and not prevCap:
-          result.add '_'
-        result.add c.toLowerAscii()
-      prevCap = true
-    else:
-      prevCap = false
-      result.add c
+        current.add(c)
+      inc i
+    if current.len != 0:
+      yield current
+
+proc toSnakeCase*(s: string, uncapitalized = false): string =
+  result = ""
+  for word in splitWordsCamelCase(s, uncapitalized):
+    if result.len != 0: result.add '_'
+    result.add word
+
+proc toKebabCase*(s: string, uncapitalized = false): string =
+  result = ""
+  for word in splitWordsCamelCase(s, uncapitalized):
+    if result.len != 0: result.add '-'
+    result.add word
